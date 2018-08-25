00bbe1e13c79f44ef59c212a6960d61817ccbe70
cgad <35946778+cgad@users.noreply.github.com>
2018-08-25 12:50:39 -0600

    define var Spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var songName = process.argv.slice(2).join(" ");
